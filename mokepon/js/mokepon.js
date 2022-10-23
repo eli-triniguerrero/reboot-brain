a373721e47484de249ac4c97621a0d5c24a927b6
eli-triniguerrero <eli.triniguerrero@gmail.com>
2022-10-22 20:34:56 -0500

    llamando primer boton

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert('Hiciste clic clic clic )))')
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota');
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
