07761d2b485f6061f2c33a3c7c55c04c4b6cbf0a
eli-triniguerrero <eli.triniguerrero@gmail.com>
2022-11-02 13:59:03 -0600

    mostrando opcion seleccionada

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,7 +4,31 @@ function iniciarJuego () {
 }
 
 function seleccionarMascotaJugador() {
-    alert('Hiciste clic clic clic )))')
+    let mascotaSeleccionada = document.querySelectorAll("input[name='mascota']");
+
+    // for ( let i= 0; i > mascotaSeleccionada.length; i++) {
+    //     if (mascotaSeleccionada[i].checked == i) {
+    //         alert('Seleccionaste ')
+    //     } else {
+    //         alert('EROOR')
+    //     }
+    // }
+    
+    if (mascotaSeleccionada[0].checked) {
+        alert('Seleccionaste a HIPODOGE');
+    } else if (mascotaSeleccionada[1].checked) {
+        alert('Seleccionaste a CAPIPEPO');
+    } else if (mascotaSeleccionada[2].checked) {
+        alert('Seleccionaste a RATIGUEYA');
+    } else if (mascotaSeleccionada[3].checked) {
+        alert('Seleccionaste a LANGOSTELVIS');
+    } else if (mascotaSeleccionada[4].checked) {
+        alert('Seleccionaste a TUCAPALMA');
+    } else if (mascotaSeleccionada[5].checked) {
+        alert('Seleccionaste a PYDOS');
+    } else {
+        alert('NO HAS SELECCIONADO NADA, SELECCIONA UNA MASCOTA!');
+    }
 }
 
 window.addEventListener('load', iniciarJuego);
